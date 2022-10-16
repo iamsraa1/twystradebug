07964825d61d1a6b8916fc90ba11ebea19728a1d
Sravan Kumar <86399834+rmkcet2022@users.noreply.github.com>
2022-10-16 14:18:06 +0530

    added function to copy ii program

--- bugbuster/css/index.js
+++ bugbuster/css/index.js
@@ -11,3 +11,16 @@ function copyToClipboard() {
     document.body.removeChild(el)
     alert('Code Copied');
   }
+  function copyToClipboard2() {
+    const str = document.getElementById('item-to-copy-2').innerText
+    const el = document.createElement('textarea')
+    el.value = str
+    el.setAttribute('readonly', '')
+    el.style.position = 'absolute'
+    el.style.left = '-9999px'
+    document.body.appendChild(el)
+    el.select()
+    document.execCommand('copy')
+    document.body.removeChild(el)
+    alert('Code Copied');
+  }
